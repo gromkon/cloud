c8b797b054bfdf3bd95f2f736196d2fcc49208e8
gromkon <steamgrom@mail.ru>
2020-10-03 19:58:28 +0300

    Добавлена возможность передачи файла от клиента к серверу

--- client/src/main/java/MainClient.java
+++ client/src/main/java/MainClient.java
@@ -11,7 +11,7 @@ public class MainClient {
     private static void serializable() {
         try (Socket socket = new Socket("localhost", 8085)) {
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            TempFileMessage tmp = new TempFileMessage(Paths.get("client/example.txt"));
+            TempFileMessage tmp = new TempFileMessage(Paths.get("client/files/example.txt"));
             out.writeObject(tmp);
         } catch (UnknownHostException e) {
             e.printStackTrace();
@@ -20,18 +20,18 @@ public class MainClient {
         }
     }
 
-    private static void binary() {
-        try (Socket socket = new Socket("localhost", 8085)) {
-            byte[] bytes = {65, 66, 67, 49, 50, 51};
-            socket.getOutputStream().write(bytes);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    private static void binary() {
+//        try (Socket socket = new Socket("localhost", 8085)) {
+//            byte[] bytes = {65, 66, 67, 49, 50, 51};
+//            socket.getOutputStream().write(bytes);
+//        } catch (UnknownHostException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public static void main(String[] args) {
-        binary();
+        serializable();
     }
 }
--- server/src/main/java/MainApp.java
+++ server/src/main/java/MainApp.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class MainApp {
 
@@ -13,35 +16,33 @@ public class MainApp {
                  ObjectInputStream in = new ObjectInputStream(socket.getInputStream())
             ) {
                 TempFileMessage tmp = (TempFileMessage) in.readObject();
-                byte[] bytes = tmp.getBytes();
-                for (byte b: bytes) {
-                    System.out.print((char) b);
-                }
+                Path path = Paths.get("server/files/" + tmp.getFileName());
+                Files.write(path, tmp.getBytes());
             }
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    private static void binary() {
-        try (ServerSocket sc = new ServerSocket(8085)) {
-            System.out.println("Server listening");
-            try (Socket socket = sc.accept();
-                 BufferedInputStream in = new BufferedInputStream(socket.getInputStream())
-            ) {
-                System.out.println("Client is received");
-                int n;
-                while ((n = in.read()) != -1) {
-                    System.out.print((char)n);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    private static void binary() {
+//        try (ServerSocket sc = new ServerSocket(8085)) {
+//            System.out.println("Server listening");
+//            try (Socket socket = sc.accept();
+//                 BufferedInputStream in = new BufferedInputStream(socket.getInputStream())
+//            ) {
+//                System.out.println("Client is received");
+//                int n;
+//                while ((n = in.read()) != -1) {
+//                    System.out.print((char)n);
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public static void main(String[] args) {
-        binary();
+        serializable();
     }
 
 }
